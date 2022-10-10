9d52fbf0c78add1439316def9aa52f0fa0107af8
JorgVargas <jorge.vargas2406@outlook.com>
2022-10-10 11:11:21 -0500

    se configuro MYSQL

--- main/settings.py
+++ main/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bddn_oficial',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
