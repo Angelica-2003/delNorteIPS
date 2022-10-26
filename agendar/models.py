949774ecb1227346c927b0ee01e9ac2bd3010d55
brayancarreo5 <brayan.carreo5@misena.edu.co>
2022-10-26 12:10:37 -0500

    se elimino algo

--- agendar/models.py
+++ agendar/models.py
@@ -3,10 +3,3 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
-
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> d3ebe77a1fc5f6aa815809e7629d032c40232f70
